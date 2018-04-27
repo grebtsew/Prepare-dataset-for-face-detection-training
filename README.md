aeb20aec3f35e0f3ec63d1b78b5eea319bdb8cb7
grebtsew <danwe980@student.liu.se>
2018-04-27 17:05:39 +0200

    Changed links to work with implementation

--- README.md
+++ README.md
@@ -2,4 +2,10 @@
 
 This python code prepare images from a dataset to be trained in tensorflow.
 
-We use dlib to detect faces in images from one file. Then generate a xml document for each file with information about the detections. Finally the result is saved in another folder.
+We use dlib to detect faces in images from all files in folder "images". Then generate a xml document for each file with information about the detections. Finally the result is saved in another folder "res".
+
+
+# Requirements:
+
+dlib
+cv2 (to show images, can remove cv2 code if unwanted)
--- object_detection_prepare_training_images_dlib.py
+++ object_detection_prepare_training_images_dlib.py
@@ -1,8 +1,7 @@
 print ("Import libraries...")
 
 import numpy as np
-import os, os.path
-import six.moves.urllib as urllib
+import os
 import sys
 import tarfile
 import tensorflow as tf
@@ -12,7 +11,6 @@ from shutil import copyfile
 
 from collections import defaultdict
 from io import StringIO
-from matplotlib import pyplot as plt
 from PIL import Image
 
 import face_recognition
@@ -22,79 +20,20 @@ import cv2
 
 FaceDetector = None
 
-
-
-# This is needed since the notebook is stored in the object_detection folder.
-sys.path.append("..")
-
-# ## Object detection imports
-# Here are the imports from the object detection module.
-
-from utils import label_map_util
-
-from utils import visualization_utils as vis_util
-
-from Own_Code import create_xml as xml
-
-
-# # Model preparation 
-
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" model here. See the [detection model zoo](https://github.com/tensorflow/models/blob/master/object_detection/g3doc/detection_model_zoo.md) for a list of other models that can be run out-of-the-box with varying speeds and accuracies.
+import create_xml as xml
 
 class file_path:
     def __init__(self):
         self.path = ""
         self.name = ""
-       
 
-# What model to download.
-#MODEL_NAME = 'ssd_mobilenet_v1_coco_11_06_2017'
-#MODEL_FILE = MODEL_NAME + '.tar.gz'
-#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
-
-# Path to frozen detection graph. This is the actual model that is used for the object detection.
-#PATH_TO_CKPT = "frozen_inference_graph_face.pb"#MODEL_NAME + '/frozen_inference_graph.pb'
-
-# List of the strings that is used to add correct label for each box.
-#PATH_TO_LABELS = os.path.join('data', 'mscoco_label_map.pbtxt')
-
-#NUM_CLASSES = 90
-
-# ## Load a (frozen) Tensorflow model into memory.
-
-print ("Loading Modell...")
+print ("Loading dlib Modell...")
 
 FaceDetector = dlib.get_frontal_face_detector()
- 
-#detection_graph = tf.Graph()
-#with detection_graph.as_default():
-#  od_graph_def = tf.GraphDef()
-#  with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
-#    serialized_graph = fid.read()
-#    od_graph_def.ParseFromString(serialized_graph)
-#    tf.import_graph_def(od_graph_def, name='')
-
-
-# ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
-
-#print ("Loading label map...")
-
-
-#label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
-#categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
-#category_index = label_map_util.create_category_index(categories)
-
 
 print ("Get files...")
 
-# For the sake of simplicity we will use only 2 images:
-# If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = 'inviduell_del/bilder(50)'
+PATH_TO_TEST_IMAGES_DIR = 'images'
 TEST_IMAGE_PATHS = []
 
 valid_images = [".jpg",".gif",".png",".tga"]
@@ -107,7 +46,7 @@ for files in os.listdir(PATH_TO_TEST_IMAGES_DIR):
   temp.name = files
   temp.path = os.path.join(PATH_TO_TEST_IMAGES_DIR,files)
   TEST_IMAGE_PATHS.append(temp)
-                              
+
 
 # Convert_dlib_box_to_OpenCV_box(box)
 # @param takes in a dlib box
@@ -116,10 +55,10 @@ def convert_dlib_box_to_openCV_box(box):
     return (int(box.left()), int(box.top()), int(box.right() - box.left()),
                          int(box.bottom() - box.top()) )
 
-# # Detection and actual loop
-
+# Detection and actual loop
 print("Detecting...")
 
+# for each image
 for image_path in TEST_IMAGE_PATHS:
 
     frame = cv2.imread(image_path.path)
@@ -131,14 +70,14 @@ for image_path in TEST_IMAGE_PATHS:
 
 
     if len(box_arr) > 0 :
-         
+
         # Convert box to OpenCV
         face_box = convert_dlib_box_to_openCV_box(box_arr[0])
 
         print(face_box)
 
 
-  
+
       # Log detection result
         print (time.strftime('%d/%m/%Y %H:%M:%S'))
 
@@ -152,7 +91,7 @@ for image_path in TEST_IMAGE_PATHS:
         print ("ymin=%s xmin=%s ymax=%s xmax=%s"  % (ymin, xmin, ymax, xmax ))
 
         new_box = xml.detected_object()
-        new_box.name = 'face'#category_index[np.squeeze(classes).astype(np.int32)[i]]['name']
+        new_box.name = 'face'
         new_box.pose = "Unspecified"
         new_box.truncated = 0
         new_box.difficult = 0
@@ -160,7 +99,8 @@ for image_path in TEST_IMAGE_PATHS:
 
         detect_box_array.append(new_box);
 
-        xml.create_training_xml('inviduell_del/bilder(50)',
+        # create xml
+        xml.create_training_xml('images',
                               image_path.name,
                               os.path.abspath(image_path.path),
                               'Unknown',
@@ -169,18 +109,18 @@ for image_path in TEST_IMAGE_PATHS:
                               len(box_arr),
                               0,
                               detect_box_array,
-                              'inviduell_del/dataset(50)')
+                              'res')
 
-              
+
         topLeft = (int(face_box[0]), int(face_box[1]))
         bottomRight = (int(face_box[0] + face_box[2]), int(face_box[1] + face_box[3]))
         cv2.rectangle(frame, topLeft,bottomRight, (0,0,255), 2,1 )
 
 
-        print("Created file %s" % (''.join(['inviduell_del/dataset(50)','/', image_path.name])))
-        copyfile(image_path.path, ''.join(['inviduell_del/dataset(50)/',image_path.name]))
-        cv2.imshow('object detection', frame)
-        cv2.waitKey(1)
+        print("Created file %s" % (''.join(['res/','/', image_path.name])))
+        copyfile(image_path.path, ''.join(['res/',image_path.name]))
+        cv2.imshow('face detection', frame)
+        cv2.waitKey(0)
     else:
-        copyfile(image_path.path, ''.join(['inviduell_del/nodetection(50)/',image_path.name]))
-        print("Created file %s" % (''.join(['inviduell_del/nodetection(50)','/', image_path.name]))) 
+        copyfile(image_path.path, ''.join(['fail/',image_path.name]))
+        print("Created file %s" % (''.join(['fail/','/', image_path.name])))
